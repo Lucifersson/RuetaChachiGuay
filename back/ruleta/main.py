61d61094bf558a335e216cceccef48a90f1388f3
Alejandro Garcés <agarces@koibox.cloud>
2025-09-24 14:01:57 +0200

    Crear endpoint de pruebas para hacer pruebas en el frontend

--- back/ruleta/main.py
+++ back/ruleta/main.py
@@ -1,6 +1,14 @@
 """ Punto de entrada de la API """
 
+# FastAPI
 from fastapi import FastAPI
+
+# Python
+import random
+
+# Ruleta
+from ruleta.environment import env_vars
+
 
 app = FastAPI()
 
@@ -8,3 +16,8 @@ app = FastAPI()
 @app.get("/")
 async def root() -> str:
     return "Funciona"
+
+
+@app.get("/saldo")
+async def generar_saldo_aleatorio() -> float:
+    return random.uniform(1000.0, 3000.0)
