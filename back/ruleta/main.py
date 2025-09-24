2d995fe862cc8905336006e583f8538cb21674fd
Alejandro Garcés <agarces@koibox.cloud>
2025-09-24 14:19:34 +0200

    Añadir middleware de cors

--- back/ruleta/main.py
+++ back/ruleta/main.py
@@ -2,6 +2,7 @@
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Python
 import random
@@ -11,6 +12,19 @@ from ruleta.environment import env_vars
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=(
+        "http://localhost",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
+    ),
+    allow_credentials=True,
+    allow_methods=("*",),
+    allow_headers=("*",),
+)
 
 
 @app.get("/")
