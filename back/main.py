b657b9986896ade81815ca20591cdb086abe2ef5
Alejandro Garcés <agarces@koibox.cloud>
2025-09-25 11:41:35 +0200

    Exponer app solamente si se ejcuta con fastapi

--- back/main.py
+++ back/main.py
@@ -1,13 +1,12 @@
 """ Punto de entrada del backend """
 
-import ruleta.app
-
-app = ruleta.app.app
-
-
 def main() -> None:
     pass # Aqui se podria configurar si se ejecuta
 
 
 if __name__ == "__main__":
     main()
+else:
+    import ruleta.app
+
+    app = ruleta.app.app
