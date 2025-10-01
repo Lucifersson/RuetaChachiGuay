9d7c688fe04899e8375272f3855a82b0b3fbc949
Alejandro Garcés <agarces@koibox.cloud>
2025-10-01 14:54:01 +0200

    Configurar el punto de entrada de FastAPI

--- back/app/app.py
+++ back/app/app.py
@@ -2,8 +2,19 @@
 # FastAPI
 from fastapi import FastAPI
 
+# Ruleta
+from app import env
 
-app = FastAPI()
+
+# Crear el punto de entrada de FastAPI
+app = FastAPI(
+    debug=env.DEBUG,
+    title="Ruleta Backend",
+    description="Backend para el frontend de la ruleta",
+)
+
+# Crear las rutas
+# Crear los middlewares
 
 
 @app.get("/")
